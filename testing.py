942567a3ed4262cd23c7fd665458379e2e4a40f7
akhilaprakash <akhila.p281@gmail.com>
2024-03-21 10:34:20 -0700

    My first commit

---
+++ testing.py
@@ -0,0 +1,3 @@
+import magic
+print(magic.from_file('/Users/akhilaprakash/Downloads/AkhilaPrakashResume.pdf'))
+print(magic.from_file('/Users/akhilaprakash/Downloads/AkhilaPrakashResume.pdf', mime = True))
